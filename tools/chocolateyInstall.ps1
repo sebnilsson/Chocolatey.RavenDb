8128356bce47fc8ebaf2c50a45b05c058fa934c3
Sebastian Nilsson <sebastian.nilsson@offerta.se>
2016-12-12 12:04:59 +0100

    Added Nuget-related files and package-history

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$url = 'http://hibernatingrhinos.com/downloads/RavenDB%20Installer/3800'
+
+Install-ChocolateyPackage 'RavenDB' 'exe' '/quiet /msicl "RAVEN_TARGET_ENVIRONMENT=DEVELOPMENT TARGETDIR=C:\ INSTALLFOLDER=C:\RavenDB RAVEN_INSTALLATION_TYPE=SERVICE REMOVE=IIS ADDLOCAL=Service"' $url
